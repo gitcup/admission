074421c62b284f00135aaca06886db5b01460c40
gitcup <40912045+gitcup@users.noreply.github.com>
2018-10-20 10:53:12 +0700

    เริ่มพัฒนาส่วนแก้ไขข้อมูล
    -สามารถปริ้นได้แล้ว

---
+++ admission-newdesign/test/menuType.php
@@ -0,0 +1,9 @@
+<h1>กรอกใบสมัครสอบออนไลน์</h1>
+<!--<div id="menutype" class="flr">
+<span><a href="<? echo "index.php?p=frmRegister1"; ?>"></a></span></div> -->
+<div class="clr"></div>
+<?php
+   if($p == "frmRegister1"){ $txt = "ปริญญาตรี"; }
+   if($p == "frmRegister2"){ $txt = "ปริญญาโท"; }
+   if($p == "frmRegister3"){ $txt = "ป.บัณฑิต"; }
+?>
